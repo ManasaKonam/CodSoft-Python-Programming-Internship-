93963adbfe8d90ea4081facf58a6d9abd3093eb9
ManasaKonam <21.421manasa@gmail.com>
2024-06-25 19:51:42 +0530

    Add files via upload

--- task2.py
+++ task2.py
@@ -1,59 +1,45 @@
-import random 
-import array 
-  
-# maximum length of password needed 
-# this can be changed to suit your password length 
-MAX_LEN = 12
-  
-# declare arrays of the character that we need in out password 
-# Represented as chars to enable easy string concatenation 
-DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']   
-LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',  
-                     'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 
-                     'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 
-                     'z'] 
-  
-UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',  
-                     'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q', 
-                     'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 
-                     'Z'] 
-
-SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',  
-           '*', '(', ')', '<'] 
-  
-# combines all the character arrays above to form one array 
-COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS 
-  
-# randomly select at least one character from each character set above 
-rand_digit = random.choice(DIGITS) 
-rand_upper = random.choice(UPCASE_CHARACTERS) 
-rand_lower = random.choice(LOCASE_CHARACTERS) 
-rand_symbol = random.choice(SYMBOLS) 
-  
-# combine the character randomly selected above 
-# at this stage, the password contains only 4 characters but  
-# we want a 12-character password 
-temp_pass = rand_digit + rand_upper + rand_lower + rand_symbol 
-  
-  
-# now that we are sure we have at least one character from each 
-# set of characters, we fill the rest of 
-# the password length by selecting randomly from the combined  
-# list of character above. 
-for x in range(MAX_LEN - 4): 
-    temp_pass = temp_pass + random.choice(COMBINED_LIST) 
-  
-    # convert temporary password into array and shuffle to  
-    # prevent it from having a consistent pattern 
-    # where the beginning of the password is predictable 
-    temp_pass_list = array.array('u', temp_pass) 
-    random.shuffle(temp_pass_list) 
-  
-# traverse the temporary password array and append the chars 
-# to form the password 
-password = "" 
-for x in temp_pass_list: 
-        password = password + x 
-          
-# print out password 
-print(password)
+def add(num1, num2):
+    return num1 + num2
+ 
+# Function to subtract two numbers
+def subtract(num1, num2):
+    return num1 - num2
+ 
+# Function to multiply two numbers
+def multiply(num1, num2):
+    return num1 * num2
+ 
+# Function to divide two numbers
+def divide(num1, num2):
+    return num1 / num2
+ 
+print("Please select operation -\n" \
+        "1. Add\n" \
+        "2. Subtract\n" \
+        "3. Multiply\n" \
+        "4. Divide\n")
+ 
+ 
+# Take input from the user
+select = int(input("Select operations form 1, 2, 3, 4 :"))
+ 
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
+ 
+if select == 1:
+    print(number_1, "+", number_2, "=",
+                    add(number_1, number_2))
+ 
+elif select == 2:
+    print(number_1, "-", number_2, "=",
+                    subtract(number_1, number_2))
+ 
+elif select == 3:
+    print(number_1, "*", number_2, "=",
+                    multiply(number_1, number_2))
+ 
+elif select == 4:
+    print(number_1, "/", number_2, "=",
+                    divide(number_1, number_2))
+else:
+    print("Invalid input")
